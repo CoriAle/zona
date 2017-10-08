da4a5994a05bf2e490ff574d9dafcfc608a342e2
Cori <1995coral@hotmail.es>
2017-10-08 12:13:38 -0600

    Urls

--- Mascotas/apps/adopcion/models.py
+++ Mascotas/apps/adopcion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    """docstring for Persona."""
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=10)
+    edad = models.IntegerField()
+    telefono = models.CharField(max_length=12)
+    corrreo = models.EmailField()
+    direccion = models.TextField()
--- Mascotas/apps/mascota/models.py
+++ Mascotas/apps/mascota/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from apps.adopcion.models import Persona
 # Create your models here.
+
+class Vacuna(models.Model):
+    """docstring for Vacuna."""
+    nombre=models.CharField(max_length=50)
+
+
+class Mascota(models.Model):
+    """docstring forMascota."""
+    nombre = models.CharField(max_length=50)
+    genero = models.CharField(max_length=10)
+    edad= models.IntegerField()
+    fecha_rescate = models.DateField()
+    persona = models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
+    vacuna = models.ManyToManyField(Vacuna, blank=True)
